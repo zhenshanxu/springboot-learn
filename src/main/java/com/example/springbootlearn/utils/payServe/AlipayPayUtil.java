fedd3289e1acdd0e32d590339c7da88b3e7fb721
xuzhenshan <xuzhenshanvip@163.com>
2024-05-13 16:04:44 +0800

    fix:初始化配置支付

---
+++ src/main/java/com/example/springbootlearn/utils/payServe/AlipayPayUtil.java
@@ -0,0 +1,23 @@
+package com.example.springbootlearn.utils.payServe;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Project springboot-learn
+ * @Description 支付宝支付
+ * @Author xuzhenshan
+ * @Date 2024/5/13 15:49:49
+ * @Version 1.0
+ */
+
+@Slf4j
+public class AlipayPayUtil {
+
+    // 链接请求
+    // web支付
+    // h5 支付
+    // 查询账单
+    // 关闭账单
+    // 申请退款
+
+}
---
+++ src/main/java/com/example/springbootlearn/utils/payServe/WechatPayUtil.java
@@ -0,0 +1,20 @@
+package com.example.springbootlearn.utils.payServe;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Project springboot-learn
+ * @Description 微信支付
+ * @Author xuzhenshan
+ * @Date 2024/5/13 15:53:45
+ * @Version 1.0
+ */
+
+@Slf4j
+public class WechatPayUtil {
+    // 加载证书
+    // 唤醒 Native支付
+    // 支付状态
+    // 关闭订单
+    // 申请退款
+}
